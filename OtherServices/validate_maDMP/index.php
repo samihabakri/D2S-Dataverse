baeb186df0c346710747099b53413269c2a69cf8
hertai86 <hertani86@gmail.com>
2019-06-02 15:49:47 +0200

    Full maDMP testing for testing. Show errors in validating.

--- OtherServices/validate_maDMP/index.php
+++ OtherServices/validate_maDMP/index.php
@@ -1,10 +1,20 @@
   <!DOCTYPE html>
   <html>
   <head>
-  	<title>
-    </title>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/ajv/6.10.0/ajv.min.js"></script>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <style>
+        form {text-align: center;}
+        body {background-color: powderblue;}
+        h1   {color: blue;
+              text-align: center}
+        h2   {text-align: center}
+        h3   {padding-left: 97px}
+        div {margin-left: 100PX}
+        #detailedResultID,#invaildresultID {color: red; font-size: 23PX}
+
+    </style>
+    <title></title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ajv/6.10.0/ajv.bundle.js"></script>
+    <script src="bundle.js"></script>
   </head>
   <body>
     <textarea rows="5" cols="5" id="vocabularyID" style="display:none;" >
@@ -21,94 +31,45 @@
         ?>
     </label>
     <br>
-    <h2>Validating maDMP against RDA DMP schema</h2>
-    <br>
-    maDMP:
-  	<form action="<?php echo $_SERVER['PHP_SELF'];?> " method="POST">
-      <br>
-      <textarea rows="20" cols="100" id="dataID"  placeholder="Paste your maDMP here please!!"></textarea><br>
+    <h1>Validating maDMP against RDA DMP schema</h2>
+    <h2>Tool for validating machine actionable data management plan against RDA DMP standard schema</h2>
+    <h3>maDMP:</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?> " method="POST">
+      <textarea style="" rows="30" cols="150" id="dataID"  placeholder="Paste your maDMP here please!!"></textarea><br>
     </form>
-    <button onclick="validate()">Test Validation</button>
-    <br><br>
-    <label id="resultID"></label>
-    <br>
-    <label id="detailedResultID"></label>
-
+    <div>
+        <button style="padding: auto; display:block" onclick="validate()">Test Validation</button><br>
+        <label id="vaildresultID"></label>
+        <label id="invaildresultID"></label><br><br>
+        <label id="detailedResultID"></label>
+    </div>
     <script type="text/javascript">
-      var maDMP;
-      var maDMPJSON;
-      var schema;
-      var schemaJSON;
-      var vocabulary;
-      var vocabularyJSON;
-
-
-
-
-      //var maDMPJSON=JSON.parse(maDMP.substring(1, maDMP.length));
-
-
-
-
-      function validate()
-      {
+      function validate(){
+        document.getElementById('vaildresultID').innerHTML = "";
+        document.getElementById('invaildresultID').innerHTML = "";
+        document.getElementById('detailedResultID').innerHTML = "";
         var maDMP = document.getElementById("dataID").value;
-        console.log("maDMP");
-        console.log(maDMP);
-
         var maDMPJSON = JSON.parse(maDMP);//.substring(1, maDMP.length));
-        console.log("maDMPJSON");
-        console.log(maDMPJSON);
-
         var schema = document.getElementById("schmaID").textContent;
-        console.log("schema");
-        console.log(schema);
-
         var schemaJSON = JSON.parse(schema.substring(1, schema.length));
-        console.log("schemaJSON");
-        console.log(schemaJSON);
-
-        var vocabulary = document.getElementById("vocabularyID").textContent;
-        console.log("vocabulary");
-        console.log(vocabulary);
-
-        var vocabularyJSON = JSON.parse(vocabulary);
-        console.log("vocabularyJSON");
-        console.log(vocabularyJSON);
-
 
         var ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
-        //ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
-    		var validate = ajv.compile(schemaJSON);
-    		var valid = validate(maDMPJSON);
-    		if (!valid) {
-          document.getElementById('resultID').innerHTML = "Not validated maDMP" ;
-          console.log("invalid");
-          validate_voc(vocabularyJSON,maDMPJSON);
+        var validate = ajv.compile(schemaJSON);
+        var valid = validate(maDMPJSON);
+        if (!valid) {
+            console.log("validate.errors");
+            console.log(validate.errors);
+            document.getElementById('invaildresultID').innerHTML = "Not validated maDMP" ;
+            document.getElementById('detailedResultID').innerHTML = JSON.stringify(validate.errors, null, 4);
+            console.log("invalid");
         }
-
         else {
-          document.getElementById('resultID').innerHTML = "Validated maDMP" ;
-          console.log("valid");
-          validate_voc(vocabulary,maDMPJSON);
+            document.getElementById('vaildresultID').innerHTML = "Validated maDMP" ;
+            console.log("valid");
         }
+
+
       }
-
-
-
-
-
-
-
-
-
-
-
-
-
-  </script>
+    </script>
 </body>
 </html>
-
-
-
